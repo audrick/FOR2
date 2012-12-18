8b7dc74722cfbc9599f1c1b706cb7f6cd8f11c93
Audric Kim <audrickim@gmail.com>
2012-12-18 18:34:58 -0500

    made map larger

---
+++ app/assets/javascripts/styles.js
@@ -0,0 +1,23 @@
+var styles = [
+    {
+      stylers: [
+        { hue: "#00ffe6" },
+        { saturation: 20 }
+      ]
+    },{
+      featureType: "road",
+      elementType: "geometry",
+      stylers: [
+        { lightness: 200 },
+        { visibility: "simplified" }
+      ]
+    },{
+      featureType: "road",
+      elementType: "labels",
+      stylers: [
+        { visibility: "off" }
+      ]
+    }
+  ];
+
+
